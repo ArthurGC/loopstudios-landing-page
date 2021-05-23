01c33137429a48ef54626f68a8e7749a48c5ae2a
arthurGC <aguzmanc@unsa.edu.pe>
2021-05-23 16:05:12 -0500

    Fix the changing of icon hamb for close

--- js/app.js
+++ js/app.js
@@ -1,7 +1,11 @@
 const hamb = document.querySelector('.navbar__hamb');
 const menu = document.querySelector('.navbar__bar');
+const iconHamb = document.querySelector('.hamburger');
+const iconClose = document.querySelector('.close');
 function openMenu() {
     menu.classList.toggle('active');
+    iconHamb.classList.toggle('hide');
+    iconClose.classList.toggle('hide');
 }
 
 hamb.addEventListener('click',openMenu);
